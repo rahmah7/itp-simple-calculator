9d1d1b77f88ec4ab5e61689cce5b2a8b34f22ab8
rahmah7 <109670013+rahmah7@users.noreply.github.com>
2024-07-29 01:54:24 +0300

    Create Calculator.py
    Lap

---
+++ Calculator.py
@@ -0,0 +1,34 @@
+x=5
+y=2
+def add(x,y):
+  return x+y
+  
+def subtract(x,y):
+  return x-y
+
+def multiply(x,y):
+  return x*y
+
+def Divide(x,y):
+  return x/y
+
+def Square(x):
+  return x**2
+
+def square(y):
+  return y**2
+
+def power(x,y)
+  return pow(x,y)
+
+def SquareRoot(x):
+  return sqrt(x)
+
+def SquareRoot(y):
+  return sqrt(y)
+
+
+  
+  
+  
+  
